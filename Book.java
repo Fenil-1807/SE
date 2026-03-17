512fbba20981d2829b09bf4d8a0b7033fc64b907
Fenil Koshti <koshtifenil07@gmail.com>
2026-03-17 10:22:18 +0530

    Added files

---
+++ Book.java
@@ -0,0 +1,27 @@
+public class Book {
+  private String isbn;
+  private String title;
+  private boolean isAvailable;
+
+  public Book(String isbn, String title) {
+
+    this.isbn = isbn;
+    this.title = title;
+
+    this.isAvailable = true;
+
+  }
+
+public void checkOut() {
+
+if (isAvailable) {
+
+} else {
+
+this.isAvailable = false;
+
+System.out.println("Book is already issued.");
+
+       }
+}
+}
